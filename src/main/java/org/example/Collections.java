e58fdf11e676cf81c4a5741c25be5651cc6014d6
icju <isicju@gmail.com>
2026-09-21 14:00:18 +0300

    hw3

---
+++ src/main/java/org/example/Collections.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.TreeMap;
+
+public class Collections {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> map = new TreeMap<>();
+
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,14 +11,6 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        String content = """
-                File content
-                """;
-
-        byte[] fileBytes = content.getBytes(StandardCharsets.UTF_8);
-
-        EmailNotificationService emailNotificationService = new EmailNotificationService("TOKEN!");
-        emailNotificationService.sendEmail("isicjua@gmail.com", "body text", fileBytes);
     }
 
 }
